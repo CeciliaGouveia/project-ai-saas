5c93099a9e50474e7d2f85ed2854d40f3d0846af
Cecilia Gouveia <cecilia_gouveia@hotmail.com>
2025-07-17 18:33:20 +0100

    Confecção da página de dashboard, integração com Clerck

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,20 @@
 import React from "react"
 import Navbar from "../components/Navbar"
 import Hero from "../components/Hero"
+import Aitools from "../components/Aitools"
+import Testimonial from "../components/Testemonials"
+import Plan from "../components/Plan"
+import Footer from "../layouts/Footer"
 
 const Home = () => {
   return (
     <>
       <Navbar />
       <Hero />
+      <Aitools />
+      <Testimonial />
+      <Plan />
+      <Footer />
     </>
   )
 }
